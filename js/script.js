5040226e9b68143fbdf9621d588ef74560bd305b
ngoquocvuong123 <ngoquocvuong645@gmail.com>
2026-06-23 15:44:19 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ if (mainTitle && welcomeText) {
     mainTitle.textContent = "Khám Phá Du Lịch Nha Trang";
     welcomeText.textContent = "Chào mừng bạn đến với vương quốc biển xanh, cát trắng và nắng vàng!";
 }
+const helloBtn = document.getElementById("helloBtn");
+const helloResult = document.getElementById("helloResult");
+
+if (helloBtn && helloResult) {
+  helloBtn.addEventListener("click", function () {
+    helloResult.textContent = "Cảm ơn bạn đã ghé thăm MyWeb!";
+  });
+}
